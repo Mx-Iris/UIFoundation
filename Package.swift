c397b9042884398f2c4d1e7959e2ede16d6df8f6
Mx-Iris <61279231+Mx-Iris@users.noreply.github.com>
2024-02-07 22:58:43 +0800

    Remove local dependencies

--- Package.swift
+++ Package.swift
@@ -1,35 +1,26 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-import Foundation
 import PackageDescription
-extension Package.Dependency {
-    static func package(localPath: String, remotePath: String, branch: String) -> Package.Dependency {
-        if FileManager.default.fileExists(atPath: localPath) {
-            return .package(path: localPath)
-        } else {
-            return .package(url: remotePath, branch: branch)
-        }
-    }
-}
 
 let package = Package(
     name: "UIFoundation",
     platforms: [.macOS(.v12), .iOS(.v15), .tvOS(.v15), .macCatalyst(.v15)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "UIFoundation",
-            targets: ["UIFoundation"]
+            targets: [
+                "UIFoundation"
+            ]
         ),
         .library(
             name: "UIFoundationToolbox",
-            targets: [ "UIFoundationToolbox"]
+            targets: [ 
+                "UIFoundationToolbox"
+            ]
         )
     ],
     dependencies: [
         .package(
-            localPath: "/Volumes/Repositories/Private/Personal/Library/Multi/FrameworkToolbox",
-            remotePath: "https://github.com/Mx-Iris/FrameworkToolbox",
+            url: "https://github.com/Mx-Iris/FrameworkToolbox",
             branch: "main"
         )
     ],
@@ -58,7 +49,9 @@ let package = Package(
 
         .testTarget(
             name: "UIFoundationTests",
-            dependencies: ["UIFoundation"]
+            dependencies: [
+                "UIFoundation"
+            ]
         ),
     ]
 )
