3cd8bd18c2f42d90126c47d7505cb5c79f670d5b
Mx-Iris <61279231+Mx-Iris@users.noreply.github.com>
2024-02-25 22:25:40 +0800

    Fix duplicate symbols error

--- Package.swift
+++ Package.swift
@@ -46,7 +46,6 @@ let package = Package(
             dependencies: [
                 "UIFoundationToolbox",
                 "UIFoundationTypealias",
-                .product(name: "AssociatedObject", package: "AssociatedObject")
             ]
         ),
 
