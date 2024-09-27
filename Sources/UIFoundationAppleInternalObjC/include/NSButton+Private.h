94c12d957d6c79c69d52be449b1d5bfb35cafe7b
Mx-Iris <61279231+Mx-Iris@users.noreply.github.com>
2024-09-27 23:38:02 +0800

    Update private classes

---
+++ Sources/UIFoundationAppleInternalObjC/include/NSButton+Private.h
@@ -0,0 +1,11 @@
+#import <AppKit/AppKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSButton (__PrivateSPI)
+
+@property (strong, nullable) NSView *contentView;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Sources/UIFoundationAppleInternalObjC/include/_NSSimpleImageView.h
@@ -0,0 +1,24 @@
+//
+//  _NSSimpleImageView.h
+//  
+//
+//  Created by JH on 2024/9/27.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <Symbols/Symbols.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface _NSSimpleImageView : NSView
+@property (readonly, nonatomic, nullable) NSImage *image;
+/// Adds a symbol effect to the image view with specified options and animation.
+- (void)addSymbolEffect:(NSSymbolEffect *)symbolEffect options:(NSSymbolEffectOptions *)options animated:(BOOL)animated API_AVAILABLE(macos(14.0)) NS_REFINED_FOR_SWIFT;
+/// Removes from the image view the symbol effect matching the type of effect passed in, with specified options and animation.
+- (void)removeSymbolEffectOfType:(NSSymbolEffect *)symbolEffect options:(NSSymbolEffectOptions *)options animated:(BOOL)animated API_AVAILABLE(macos(14.0)) NS_REFINED_FOR_SWIFT;
+/// Sets the symbol image on the image view with a symbol content transition and specified options.
+/// Passing in a non-symbol image will result in undefined behavior.
+- (void)setSymbolImage:(NSImage *)symbolImage withContentTransition:(NSSymbolContentTransition *)transition options:(NSSymbolEffectOptions *)options API_AVAILABLE(macos(14.0)) NS_REFINED_FOR_SWIFT;
+@end
+
+NS_ASSUME_NONNULL_END
