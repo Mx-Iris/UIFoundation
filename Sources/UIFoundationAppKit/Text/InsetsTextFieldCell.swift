fc37e6459b0a05d3c430b658892f507d277a975d
Mx-Iris <61279231+Mx-Iris@users.noreply.github.com>
2025-03-02 21:58:33 +0800

    Updates

--- Sources/UIFoundationAppKit/Text/InsetsTextFieldCell.swift
+++ Sources/UIFoundationAppKit/Text/InsetsTextFieldCell.swift
@@ -4,43 +4,31 @@ import AppKit
 import UIFoundationToolbox
 
 open class InsetsTextFieldCell: NSTextFieldCell {
-    open var contentInsets: NSEdgeInsets = .box.zero {
-        didSet {}
-    }
+    open var contentInsets: NSEdgeInsets = .box.zero
 
     open override func cellSize(forBounds rect: NSRect) -> NSSize {
-        var size = super.cellSize(forBounds: rect)
-        size.height += contentInsets.top + contentInsets.bottom
-        size.width += contentInsets.left + contentInsets.right
-        return size
+        super.cellSize(forBounds: rect.box.inset(by: contentInsets))
     }
 
     open override func drawingRect(forBounds rect: NSRect) -> NSRect {
-        let rect = super.drawingRect(forBounds: rect)
-        return rect.box.inset(by: contentInsets)
+        super.drawingRect(forBounds: rect.box.inset(by: contentInsets))
     }
 
     open override func titleRect(forBounds rect: NSRect) -> NSRect {
-        let rect = super.titleRect(forBounds: rect)
-        return rect.box.inset(by: contentInsets)
+        super.titleRect(forBounds: rect.box.inset(by: contentInsets))
     }
 
     open override func edit(withFrame rect: NSRect, in controlView: NSView, editor textObj: NSText, delegate: Any?, event: NSEvent?) {
-        let insetRect = rect.box.inset(by: contentInsets)
-        super.edit(withFrame: insetRect, in: controlView, editor: textObj, delegate: delegate, event: event)
+        super.edit(withFrame: rect.box.inset(by: contentInsets), in: controlView, editor: textObj, delegate: delegate, event: event)
     }
 
     open override func select(withFrame rect: NSRect, in controlView: NSView, editor textObj: NSText, delegate: Any?, start selStart: Int, length selLength: Int) {
-        let insetRect = rect.box.inset(by: contentInsets)
-        super.select(withFrame: insetRect, in: controlView, editor: textObj, delegate: delegate, start: selStart, length: selLength)
+        super.select(withFrame: rect.box.inset(by: contentInsets), in: controlView, editor: textObj, delegate: delegate, start: selStart, length: selLength)
     }
 
     open override func drawInterior(withFrame cellFrame: NSRect, in controlView: NSView) {
-        let insetRect = cellFrame.box.inset(by: contentInsets)
-        super.drawInterior(withFrame: insetRect, in: controlView)
+        super.drawInterior(withFrame: rect.box.inset(by: contentInsets), in: controlView)
     }
 }
-
-
 
 #endif
