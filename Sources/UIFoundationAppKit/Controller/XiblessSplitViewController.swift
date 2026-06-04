6bb2a59c56c4a994905fca6503ac46712222ecc4
Mx-Iris <mxiris@126.com>
2026-06-05 00:20:37 +0800

    refactor(AppKit): override nib init on Xibless split/tab controllers
    Forces nibName/bundle to nil so framework-driven instantiation
    paths can't reintroduce a nib backing, matching the rest of the
    Xibless* family.

--- Sources/UIFoundationAppKit/Controller/XiblessSplitViewController.swift
+++ Sources/UIFoundationAppKit/Controller/XiblessSplitViewController.swift
@@ -7,6 +7,10 @@ open class XiblessSplitViewController: NSSplitViewController {
         super.init(nibName: nil, bundle: nil)
     }
 
+    public override init(nibName nibNameOrNil: NSNib.Name?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
     @available(*, unavailable)
     public required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
--- Sources/UIFoundationAppKit/Controller/XiblessTabViewController.swift
+++ Sources/UIFoundationAppKit/Controller/XiblessTabViewController.swift
@@ -6,6 +6,10 @@ open class XiblessTabViewController: NSTabViewController {
     public init() {
         super.init(nibName: nil, bundle: nil)
     }
+    
+    public override init(nibName nibNameOrNil: NSNib.Name?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nil, bundle: nil)
+    }
 
     @available(*, unavailable)
     public required init?(coder: NSCoder) {
